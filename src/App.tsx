2b771cf52678807be537657776a6ac7bece8a428
Meshate <li19638176@gmail.com>
2024-09-04 12:00:50 +0800

    chore: save

--- src/App.tsx
+++ src/App.tsx
@@ -15,27 +15,38 @@ interface zfHistory {
     failCnt: number;
 }
 
+const initHistory = {
+    succCnt: 0,
+    failCnt: 0,
+}
+
 interface historyMap {
-    [zf: number]: zfHistory;
+    [zf: string]: zfHistory;
 }
 
 function History({history}: { history: historyMap }) {
-    return (
-        <div className="flex flex-col">
-            {Object.keys(history).map(zf => (
-                <div key={zf}>{zf} {'>'} {zf + 1}: {history[zf].succCnt}/{history[zf].failCnt}</div>
-            ))}
-        </div>
-    )
+    if (Object.entries(history).length > 0) {
+        return (
+            <div
+                className="flex flex-col text-left text-xl text-sky-400 border-2 ml-8 border-emerald-300 rounded min-w-20">
+                {Object.entries(history).map(([zf, h]: [string, zfHistory]) => {
+                    const next = Number(zf) + 1;
+                    return (<div className={`border border-emerald-300 py-2.5 px-6`}
+                                 key={zf}>{`【${zf} ➡️ ${next}】: ${h.succCnt} / ${h.failCnt}`}</div>)
+                })}
+            </div>
+        )
+    } else {
+        return (<div
+            className={`flex flex-col text-left text-xl text-sky-400 border-2 ml-8 border-emerald-300 rounded p-6`}>没有数据</div>)
+    }
 }
 
 function App() {
-    const [zf, setZf] = useImmer(0)
-    const [state, setState] = useImmer('')
-    const [loading, setLoading] = useImmer(false);
-    const [, setCount] = useImmer(0)
-    const [history, setHistory] = useImmer({})
-    // const [highest, setHighest] = useImmer(0)
+    const [zf, setZf] = useImmer<number>(0)
+    const [state, setState] = useImmer<string>('')
+    const [loading, setLoading] = useImmer<boolean>(false);
+    const [history, setHistory] = useImmer<historyMap>({})
 
     const probability: Record<number, number> = {
         0: 100,
@@ -81,13 +92,24 @@ function App() {
         }
         await new Promise(resolve => setTimeout(resolve, t));
 
-        setCount((c) => c + 1);
+        setHistory(draft => {
+            if (!draft[zf]) {
+                draft[zf] = {...initHistory};
+            }
+        });
+
         if (judgeZengfu(zf)) {
-            setZf((count) => count + 1)
-            setState('成功')
+            setHistory(draft => {
+                draft[zf].succCnt += 1;
+            });
+            setZf(zf => zf + 1);
+            setState('成功');
         } else {
-            setZf(judgeDrop(zf))
-            setState('失败')
+            setHistory(draft => {
+                draft[zf].failCnt += 1;
+            });
+            setZf(judgeDrop(zf));
+            setState('失败');
         }
 
         setLoading(false);
@@ -95,32 +117,34 @@ function App() {
 
     return (
         <Fragment>
-            <div className="w-screen h-screen bg-gray-900 top-0">
+            <div className="w-screen h-screen bg-gray-900">
                 <Title>吴翔翔的神秘增幅器</Title>
-                <div className="flex items-center justify-center space-x-20 p-2">
+                <div className="flex items-center pr-16">
                     <History history={history}/>
-                    <div className="flex items-center justify-center space-x-20 p-2">
-                        <b className={`text-4xl flex-none
+                    <div className={`flex flex-col flex-auto items-center justify-center`}>
+                        <div className="flex flex-auto items-center justify-center space-x-10 p-2">
+                            <b className={`text-4xl flex-none
                 ${zf < 3 ? 'text-white' : zf < 5 ? 'text-yellow-100' : zf < 9 ? 'text-yellow-200' : zf < 13 ? 'text-blue-400' : zf < 15 ? 'text-pink-300' : 'text-yellow-500'}`}>+{zf}吴翔翔</b>
-                        <img height={200} width={200} src={wx}
-                             className={`wx w-52 h-52 rounded-3xl flex-none
+                            <img height={200} width={200} src={wx}
+                                 className={`wx w-52 h-52 items-center rounded-3xl flex-none
                      ${loading ? (zf < 10 ? 'animate-shake_low' : 'animate-shake_high') : ''}`} alt="wx logo"/>
-                    </div>
-                    <div className="flex flex-col items-center justify-center space-y-10 p-2">
-                        <div
-                            className={`text-center text-pink-300 text-xl`}>当前增幅成功率: {getProbability(zf) * 1.05 + 1 < 100 ? getProbability(zf) * 1.05 + 1 : 100}%
                         </div>
-                        <div
-                            className={"w-52 h-36 p-5 rounded-l text-center text-red-600 text-4xl m-6 flex-1"}>{state && `增幅${state}`}</div>
-                        <button onClick={chong}
-                                className={`w-32 h-14  text-center text-xl transition-transform border-dotted rounded-xl ${
-                                    loading
-                                        ? 'bg-gray-400 text-white cursor-not-allowed opacity-80'
-                                        : 'bg-blue-400 text-white hover:scale-125 border-b-blue-800'
-                                }`}
-                                disabled={loading}>
-                            {loading ? '增幅中...' : '增幅'}
-                        </button>
+                        <div className="flex flex-col items-center left-0 space-y-6 p-2">
+                            <div
+                                className={`text-center text-pink-300 text-xl`}>当前增幅成功率: {getProbability(zf) * 1.05 + 1 < 100 ? getProbability(zf) * 1.05 + 1 : 100}%
+                            </div>
+                            <div
+                                className={"min-w-52 h-36 p-8 rounded-l text-center text-red-600 text-4xl flex-1"}>{state && `增幅${state}`}</div>
+                            <button onClick={chong}
+                                    className={`w-32 h-14  text-center text-xl transition-transform border-dotted rounded-xl ${
+                                        loading
+                                            ? 'bg-gray-400 text-white cursor-not-allowed opacity-80'
+                                            : 'bg-blue-400 text-white hover:scale-125 border-b-blue-800'
+                                    }`}
+                                    disabled={loading}>
+                                {loading ? '增幅中...' : '增幅'}
+                            </button>
+                        </div>
                     </div>
                 </div>
             </div>
